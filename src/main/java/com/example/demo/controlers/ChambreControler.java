fc2c1af62286703b4d7414f52025959c5e21f19b
majed1458 <majedbenkhalifa99@gmail.com>
2021-02-06 23:02:43 +0100

    hallila l9a7ba

--- src/main/java/com/example/demo/controlers/ChambreControler.java
+++ src/main/java/com/example/demo/controlers/ChambreControler.java
@@ -2,6 +2,7 @@
 package com.example.demo.controlers;
 
 import java.util.List;
+
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -11,11 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.example.demo.dao.Cambre;
 import com.example.demo.entities.Chambre;
+import com.example.demo.entities.Lit;
+
+
 
 @Controller
 
@@ -83,6 +89,8 @@ public class ChambreControler {
 		
 		return "redirect:liste";		
 	}	
+	
+
 	
 	
 }
